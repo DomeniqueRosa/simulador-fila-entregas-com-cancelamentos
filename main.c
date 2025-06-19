e6525a66b8103592478db76b17aa5eee1b9769c1
Alires D M Rosa <108585145+DomeniqueRosa@users.noreply.github.com>
2025-06-19 23:07:47 +0000

    feat: funcao entregar pedido e gerar estatisticas de entregas

--- main.c
+++ main.c
@@ -2,10 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include "./pedidos/pedidos.h"
-
+#include "./entregas/entregas.h"
 int main() {
     // Inicializa a fila e a armazenar na variavel fila
-    FilaPedidos* fila = fila_inicializar();
+    Fila* fila = fila_inicializar();
+
 
     listar_pedidos_fila(fila);
     // Verificar se a fila foi inicializada corretamente
@@ -26,7 +27,32 @@ int main() {
     cadastrar_pedido(fila, &clienteB, 2,"Produto Y", 200.75);
     cadastrar_pedido(fila, &clienteC, 3,"Produto Z", 150.00);
 
+    // Exibir pedidos na fila antes da entrega
+    printf("\nPedidos na fila antes da entrega:\n");
     // Listar pedidos na fila
     listar_pedidos_fila(fila);
+
+    Estatisticas est = {0, 0.0};
+
+    // Realizar entrega de um pedido (remover o primeiro pedido da fila)
+    printf("\nRealizando entrega do primeiro pedido...\n");
+    int resultado = realizar_entrega(fila, &est);
+    
+    if (resultado) {
+        printf("\nEntrega realizada com sucesso!\n");
+    } else {
+        printf("\nFalha ao realizar a entrega.\n");
+    }
+
+    // Exibir estatísticas após a entrega
+    printf("\nEstatísticas após a entrega:\n");
+    printf("Pedidos entregues: %d\n", est.pedidos_entregues);
+    printf("Valor total das entregas: %.2f\n", est.valor_entregues);
+
+    // Exibir pedidos na fila após a entrega
+    printf("\nPedidos na fila após a entrega:\n");
+    listar_pedidos_fila(fila);
+
+    return 0;
 
 }
